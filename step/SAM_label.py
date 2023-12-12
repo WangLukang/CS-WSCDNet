99d99afd2ef76cf97042f5d225ce24322f4cc48a
WangLukang <38554886+WangLukang@users.noreply.github.com>
2023-12-12 14:45:25 +0800

    Update SAM_label.py

--- step/SAM_label.py
+++ step/SAM_label.py
@@ -73,12 +73,12 @@ def SAM(args):
             )
 
             mask_ = np.zeros((256,256))
-            for mask in masks:
-                mask = mask.cpu().numpy()
-            # mask = np.array(mask)
-            mask = mask.astype(np.uint8)[0,:,:]
-            mask[mask == 1] = 255
-            mask_ = mask_ + mask
+            for mask1 in masks:
+                mask1 = mask.cpu().numpy()
+                # mask1 = np.array(mask1)
+                mask1 = mask1.astype(np.uint8)[0,:,:]
+                mask1[mask1 == 1] = 255
+                mask_ = mask_ + mask1
         
         #overlaps
         count = np.sum(mask == mask_)
